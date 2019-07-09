31441cc9feb5e0c149adc07f4664ac6403674dd2
mochibot <amusaur@gmail.com>
2019-07-09 14:33:14 -0700

    WIP: basical calculator functionality working

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
   return (
-    <>
+    <div onClick={() => props.inputCalc(props.operator.value)}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.operator.char}
+    </div>
   );
 };
+
+export default OperatorButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
+import SpecialButton from './SpecialButton';
 //import any components needed
-
+import { specials } from '../../../data';
 //Import your array data to from the provided data file
 
-const Specials = () => {
+const Specials = (props) => {
   // STEP 2 - add the imported data to state
-
+  const [specialBtns] = useState(specials);
   return (
-    <div>
+    <div className='calculatorSpecials'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {specialBtns.map(item => <SpecialButton inputCalc={props.inputCalc} special={item}/>)}
     </div>
   );
 };
+
+export default Specials;
